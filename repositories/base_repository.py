00034138babb594e9135b6851836bc23d9250645
211225347 <211225347@mycput.ac.za>
2026-04-27 10:58:18 +0200

    Update base_repository.py

--- repositories/base_repository.py
+++ repositories/base_repository.py
@@ -1,1 +1,29 @@
 
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar, List, Optional
+
+T = TypeVar("T")
+ID = TypeVar("ID")
+
+
+class Repository(ABC, Generic[T, ID]):
+    @abstractmethod
+    def save(self, entity: T) -> None:
+        """Create or update an entity"""
+        pass
+
+    @abstractmethod
+    def find_by_id(self, entity_id: ID) -> Optional[T]:
+        """Retrieve a single entity by ID"""
+        pass
+
+    @abstractmethod
+    def find_all(self) -> List[T]:
+        """Retrieve all entities"""
+        pass
+
+    @abstractmethod
+    def delete(self, entity_id: ID) -> None:
+        """Delete an entity by ID"""
+        pass
+``
